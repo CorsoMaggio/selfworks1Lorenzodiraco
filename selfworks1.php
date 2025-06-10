017f6bfd796ebc8670813b419703510d376e7fe3
Gamandorf <lorenzodiraco1994@gmail.com>
2025-06-10 10:46:47 +0200

    selfworks1

---
+++ selfworks1.php
@@ -0,0 +1,14 @@
+<?php
+
+$numero = 333.33;
+$arrotondo = round($numero = 333);
+
+echo $arrotondo;
+const BUDGET = 1000;
+
+
+if ($budget <= 1000) {
+    echo "paga in rate da $arrotondo";
+} else {
+    echo "Va bene";
+}
